d92dfcf93d09d41739e07d22ba5919050fb85c26
romariosilva <1994romariosilvanunes@gmail.com>
2023-09-28 22:50:20 -0300

    etapa1

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da sua conta!");
+        numero = input.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = input.next();1
+
+        System.out.println("Por favor, digite seu nome!");
+        nomeCliente = input.next();
+
+        System.out.println("Por favor, insira !");
+        saldo = input.nextDouble();
+
+        
+
+
+
+
+
+
     }
 }
