ae2812c63dc0ad657bf56264e30038132b03c5cc
romariosilva <1994romariosilvanunes@gmail.com>
2023-10-02 22:48:34 -0300

    finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
 
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        int numero;
-        String agencia;
-        String nomeCliente;
-        double saldo;
 
-        System.out.println("Por favor, digite o número da sua conta!");
-        numero = input.nextInt();
 
-        System.out.println("Por favor, digite o número da Agência!");
-        agencia = input.next();1
-
-        System.out.println("Por favor, digite seu nome!");
-        nomeCliente = input.next();
-
-        System.out.println("Por favor, insira !");
-        saldo = input.nextDouble();
-
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args)  {
+          try (Scanner texto = new Scanner(System.in)) {
+            int numero;
+            String agencia;
+            String nomeCliente;
+            double saldo;
+           
         
+            System.out.println("Por favor, digite o número da sua conta!");
+            numero = texto.nextInt();
+            texto.nextLine();
 
-
-
-
-
-
+            System.out.println("Por favor, digite o número da Agência!");
+            agencia = texto.nextLine();
+           
+            System.out.println("Por favor, digite seu nome!");
+            nomeCliente  = texto.nextLine();
+            
+            System.out.println("Por favor, digite o valor que deseja depositar!");
+            saldo = texto.nextDouble();
+            
+            System.out.println("Olá " +nomeCliente+ " obrigado por criar uma conta em nosso banco, sua agência é " +agencia+ ", conta  " +numero+ "  e seu saldo  " +saldo+ "  já está disponível para saque.");
+            
+          }     
     }
 }
